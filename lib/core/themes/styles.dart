ac58bf33a1fe467403a0500a4d79238757833c66
Mina_Emad <mennatallah.emad265@gmail.com>
2024-10-01 18:23:36 +0300

    add style page to all project

---
+++ lib/core/themes/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+abstract class StyleApp {
+  static TextStyle textStyle15 = TextStyle(
+      color: ColorApp.primaryColor, fontSize: 15, fontWeight: FontWeight.w600);
+  static TextStyle textStyle24 =
+      TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold);
+}
